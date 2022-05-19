0cda271571cf33c1ad6aa77e02a0a3bf1be94978
Igor Novak <niknovigr@gmail.com>
2022-05-19 18:22:56 +0300

    My first commit

---
+++ src/Caesar/Main.java
@@ -0,0 +1,55 @@
+package Caesar;
+
+public class Main {
+    public static void main(String[] args) {
+        alphabet();
+    }
+
+    public static void alphabet() {
+        char[] alphabet = new char[40];
+        alphabet[0] = 'A';
+        alphabet[1] = 'Б';
+        alphabet[2] = 'В';
+        alphabet[3] = 'Г';
+        alphabet[4] = 'Д';
+        alphabet[5] = 'Е';
+        alphabet[6] = 'Ё';
+        alphabet[7] = 'Ж';
+        alphabet[8] = 'З';
+        alphabet[9] = 'И';
+        alphabet[10] = 'Й';
+        alphabet[11] = 'К';
+        alphabet[12] = 'Л';
+        alphabet[13] = 'М';
+        alphabet[14] = 'Н';
+        alphabet[15] = 'О';
+        alphabet[16] = 'П';
+        alphabet[17] = 'Р';
+        alphabet[18] = 'С';
+        alphabet[19] = 'Т';
+        alphabet[20] = 'У';
+        alphabet[21] = 'Ф';
+        alphabet[22] = 'Х';
+        alphabet[23] = 'Ц';
+        alphabet[24] = 'Ч';
+        alphabet[25] = 'Ш';
+        alphabet[26] = 'Щ';
+        alphabet[27] = 'Ъ';
+        alphabet[28] = 'Ы';
+        alphabet[29] = 'Ь';
+        alphabet[30] = 'Э';
+        alphabet[31] = 'Ю';
+        alphabet[32] = 'Я';
+        alphabet[33] = '.';
+        alphabet[34] = ',';
+        alphabet[35] = '“';
+        alphabet[36] = ':';
+        alphabet[37] = '-';
+        alphabet[38] = '!';
+        alphabet[39] = '?';
+        for (int i = 0; i < alphabet.length; i++) {
+            System.out.println(alphabet[i]);
+
+        }
+    }
+}
